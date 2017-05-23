d79c93af5fedf62a4be7a77751602bc4e39a18e5
Fabian Graf <Fabi995@users.noreply.github.com>
2017-05-23 13:03:30 +0200

    Removed Abstract Form

--- files/lib/acp/form/TimelineEditForm.class.php
+++ files/lib/acp/form/TimelineEditForm.class.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace wcf\acp\form;
-
 use wcf\data\timeline\Timeline;
 use wcf\data\timeline\TimelineAction;
-use wcf\form\AbstractForm;
 use wcf\form\MessageForm;
 use wcf\system\exception\IllegalLinkException;
 use wcf\system\WCF;
 use wcf\util\DateUtil;
-
 /**
  * Shows the timeline edit form.
  *
@@ -22,40 +18,40 @@ class TimelineEditForm extends TimelineAddForm {
 	 * @inheritDoc
 	 */
 	public $activeMenuItem = 'wcf.acp.menu.link.timeline';
-	
+
 	/**
 	 * timeline id
 	 *
 	 * @var        integer
 	 */
 	public $timelineID = 0;
-	
+
 	/**
 	 * timeline object
 	 *
 	 * @var        Timeline
 	 */
 	public $timelineObj = null;
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	public function readParameters() {
 		parent::readParameters();
-		
+
 		if (isset($_REQUEST['id'])) $this->timelineID = intval($_REQUEST['id']);
 		$this->timelineObj = new Timeline($this->timelineID);
 		if (!$this->timelineObj->timelineID) {
 			throw new IllegalLinkException();
 		}
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	public function save() {
 		MessageForm::save();
-		
+
 		// update timeline
 		$this->objectAction = new TimelineAction(array($this->timelineID), 'update', array(
 			'data' => array_merge($this->additionalFields, array(
@@ -67,19 +63,19 @@ class TimelineEditForm extends TimelineAddForm {
 			))
 		));
 		$this->objectAction->executeAction();
-		
+
 		$this->saved();
-		
+
 		// show success message
 		WCF::getTPL()->assign('success', true);
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	public function readData() {
 		parent::readData();
-		
+
 		if (empty($_POST)) {
 			$this->subject = $this->timelineObj->title;
 			$this->icon = $this->timelineObj->icon;
@@ -93,13 +89,13 @@ class TimelineEditForm extends TimelineAddForm {
 			else $this->isHighlight = 0;
 		}
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	public function assignVariables() {
 		parent::assignVariables();
-		
+
 		WCF::getTPL()->assign(array(
 			'timeline' => $this->timelineObj,
 			'action' => 'edit'
